78b20cf6c2823f2f89f4cc33f538680998d61f01
FerPeke <ferna9752@gmail.com>
2021-03-26 11:25:41 -0600

    Update navbar.php

--- view/navbar.php
+++ view/navbar.php
@@ -1,35 +1,34 @@
-
-<div class="container">
-    <div class="row p-0">
-      <div class="col-12">
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="c-nav-list__link--logo" href="/" aria-label="Home" style="position: absolute; top: 25px;">
-                  <div class="c-logo">
-                    <div class="c-logo__eye">
-                      <div class="c-logo__iris"></div>
-                    </div>
-                  </div>
+<div class="container-fluid mt-2">
+  <div class="row">
+    <div class="col">
+      <header class="barra">
+        <nav class="navbar navbar-expand navbar-light bg-light">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item dropdown">
+                <a class="nav-link nav-dropdown" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Home
                 </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
+                <a style="margin-left: 10px;" class="nav-link" href="#">About</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a style="margin-left: 10px;" class="nav-link" href="#">Wip</a>
               </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px;" class="nav-link" href="#">Contact</a>
             </ul>
           </div>
         </nav>
-      </div>
-    </div>
 
+      </header>
+    </div>
   </div>
+</div>
